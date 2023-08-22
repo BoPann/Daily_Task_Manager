4b3491bfdd049c3fdaf3871540ec0b5c664ba725
BoPann <114901461+BoPann@users.noreply.github.com>
2023-08-22 15:24:29 -0700

    add the funciton of adding list

--- index.js
+++ index.js
@@ -6,15 +6,19 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const allItems = [];
+
 app.get("/", (req, res) => {
-   
-    res.render("index.ejs");
-    
+  res.render("index.ejs", {itemList: allItems}); 
   });
 
-// app.post("/submit", (req, res) => {
+app.post("/submit", (req, res) => {
+  const newItem = req.body.item;
+  allItems.push(newItem);
+  res.render("index.ejs", {itemList: allItems});
+  
+});
 
-// }
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
   });
