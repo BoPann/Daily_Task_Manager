7d060869e394b19c4061d3d5d20df14d78d26871
BoPann <114901461+BoPann@users.noreply.github.com>
2023-08-27 16:26:06 -0700

    add delete function

--- index.js
+++ index.js
@@ -7,17 +7,31 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const allItems = [];
+var nums = 0;
 
 app.get("/", (req, res) => {
-  res.render("index.ejs", {itemList: allItems}); 
+  res.render("index.ejs", {itemList: allItems,
+                              num: nums}); 
   });
 
 app.post("/submit", (req, res) => {
+  nums++;
   const newItem = req.body.item;
   allItems.push(newItem);
-  res.render("index.ejs", {itemList: allItems});
+  res.render("index.ejs", {itemList: allItems,
+    num: nums});
   
 });
+
+
+app.post("/remove", (req, res) => {
+  const index = req.body.targetItem;
+  nums--;
+  allItems.splice(index,1);
+  res.redirect("/");
+});
+
+
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
